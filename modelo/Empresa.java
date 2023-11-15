9b7cecfe8fc978e67f77ce9049b9c56613d5f6c9
sbeae <130028710+sbeae@users.noreply.github.com>
2023-11-15 19:54:28 -0300

    Add files via upload

---
+++ modelo/Empresa.java
@@ -0,0 +1,7 @@
+package modelo;
+
+public class Empresa {
+
+	private Arraylist<trabajador> trabajadores;
+
+}
---
+++ modelo/Persona.java
@@ -0,0 +1,9 @@
+package modelo;
+
+public class Persona {
+
+	private string nombre;
+	private string apellido;
+	private string rut;
+
+}
