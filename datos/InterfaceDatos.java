3a14f24db5aeedc1ebb2e195add00701a24ae7d9
sbeae <130028710+sbeae@users.noreply.github.com>
2023-11-15 19:50:21 -0300

    Add files via upload

---
+++ datos/InterfaceDatos.java
@@ -0,0 +1,20 @@
+package datos;
+
+public class InterfaceDatos {
+
+	public void read() {
+		// TODO - implement InterfaceDatos.read
+		throw new UnsupportedOperationException();
+	}
+
+	public void open() {
+		// TODO - implement InterfaceDatos.open
+		throw new UnsupportedOperationException();
+	}
+
+	public void close() {
+		// TODO - implement InterfaceDatos.close
+		throw new UnsupportedOperationException();
+	}
+
+}
